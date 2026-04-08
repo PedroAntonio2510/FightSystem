e20b9c22410b266006b269a2f001bf383b1e6ae9
PedroAntonio2510 <pedroantoniofla10@gmail.com>
2026-04-08 16:00:17 -0300

    feat: add v2 of my db

--- src/main/resources/db/migration/V1__create_tables.sql
+++ src/main/resources/db/migration/V1__create_tables.sql
@@ -1,3 +1,10 @@
+CREATE TABLE gym (
+    id serial primary key,
+    name varchar(100) not null,
+    address varchar(100),
+    created_at timestamp
+);
+
 CREATE TABLE member (
     id serial primary key,
     name varchar(100) not null,
@@ -5,8 +12,10 @@ CREATE TABLE member (
     email varchar(155) not null ,
     birth_date date not null ,
     belt varchar(20),
+    gym_id int,
     created_at timestamp,
-    updated_at timestamp
+    updated_at timestamp,
+    FOREIGN KEY (gym_id) REFERENCES gym(id)
 );
 
 CREATE TABLE plan (
@@ -14,7 +23,21 @@ CREATE TABLE plan (
     name varchar(100),
     price numeric(10,2),
     description text,
-    month_duration integer
+    gym_id int,
+    month_duration integer,
+    FOREIGN KEY (gym_id) REFERENCES gym(id)
+);
+
+CREATE TABLE class (
+    id serial primary key,
+    gym_id int,
+    instructor_id int,
+    name varchar(50),
+    class_type varchar(50),
+    start_time timestamp,
+    created_at timestamp,
+    updated_at timestamp,
+    FOREIGN KEY (gym_id) REFERENCES gym(id)
 );
 
 CREATE TYPE status AS ENUM ('ACTIVE', 'INACTIVE', 'CANCELLED');
---
+++ src/main/resources/db/migration/V2__create_table_users.sql
@@ -0,0 +1,8 @@
+CREATE TABLE users(
+        id serial primary key,
+        name varchar(100) not null,
+        email varchar(100) not null,
+        role varchar(50) not null,
+        created_at timestamp,
+        updated_at timestamp
+);
